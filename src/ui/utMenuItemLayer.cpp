0f83a74ada66f6c4564b11f1e68d2e1afbfae3e3
deaen <84744885+deaen@users.noreply.github.com>
2024-07-03 00:05:09 +0300

    im losing it

--- src/ui/utMenuItemLayer.cpp
+++ src/ui/utMenuItemLayer.cpp
@@ -83,11 +83,11 @@ CCMenu* utMenuItemLayer::createExtraMenu(CCNode* target) {
     }
     if (Mod::get()->getSettingValue<bool>("Achievements")) utButton::create("Achievements", target, menu_selector(MenuLayer::onAchievements), "ut-achievements-button"_spr, menu);
     if (Mod::get()->getSettingValue<bool>("Stats")) utButton::create("Stats", target, menu_selector(MenuLayer::onStats), "ut-stats-button"_spr, menu);
+    #ifndef GEODE_IS_MACOS
     if (Mod::get()->getSettingValue<bool>("Search")) utButton::create("Search", target, menu_selector(CreatorLayer::onOnlineLevels), "ut-search-button"_spr, menu);
     if (Mod::get()->getSettingValue<bool>("Saved")) utButton::create("Saved", target, menu_selector(CreatorLayer::onSavedLevels), "ut-saved-button"_spr, menu);
     if (Mod::get()->getSettingValue<bool>("Create")) utButton::create("Create", target, menu_selector(CreatorLayer::onMyLevels), "ut-create-button"_spr, menu);
     if (Mod::get()->getSettingValue<bool>("Featured")) utButton::create("Featured", target, menu_selector(CreatorLayer::onFeaturedLevels), "ut-featured-button"_spr, menu);
-    #ifndef GEODE_IS_MACOS
     if (Mod::get()->getSettingValue<bool>("Daily")) utButton::create("Daily", target, menu_selector(CreatorLayer::onDailyLevel), "ut-daily-button"_spr, menu);
     if (Mod::get()->getSettingValue<bool>("Weekly")) utButton::create("Weekly", target, menu_selector(CreatorLayer::onWeeklyLevel), "ut-weekly-button"_spr, menu);
     #endif
